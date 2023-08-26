04ce25c56781953b0e2e08b9066269e0d735aba0
arypru <arapruy@gmail.com>
2023-08-26 18:02:20 -0300

    FRONTEND: Add Componente Titulo

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-titulo Titulo="Transferencias"></v-titulo>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VTitulo from "@/components/VTitulo";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    VTitulo,
   }
 }
 </script>
