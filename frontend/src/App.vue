4bf5db18061ac954d262edaca39975bc1101b58a
arypru <arapruy@gmail.com>
2023-08-26 18:30:18 -0300

    FRONTEND: Add Titulo

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-titulo Titulo="Transferencias"></v-titulo>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VTitulo from "@/components/VTitulo";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    VTitulo,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ frontend/src/components/VTitulo.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1 class="blue">{{ Titulo }}</h1>
+</template>
+
+<script>
+export default {
+  props: {
+    Titulo: {
+      type: String,
+      required: true,
+    }
+  }
+}
+</script>
+
+<style>
+h1{
+  letter-spacing: 0.06em;
+}
+</style>
