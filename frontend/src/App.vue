78f89f8df4f692aebaa287af0209346eaa2f84e3
arypru <arapruy@gmail.com>
2023-08-28 20:25:43 -0300

    FRONTEND: Instalar Vuetify

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <v-titulo Titulo="Transferencias"></v-titulo>
-    <v-subtitulo subtitulo="Prueba de transferencia"></v-subtitulo>
-    <v-label-input textoLabel="prueba label input"></v-label-input>
-  </div>
+    <v-main>
+      <router-view/>
+    </v-main>
 </template>
 
 <script>
-import VTitulo from "@/components/VTitulo";
-import VSubtitulo from "@/components/VSubtitulo";
-import VLabelInput from "@/components/VLabelInput"
 
 export default {
   name: 'App',
-  components: {
-    VTitulo,
-    VSubtitulo,
-    VLabelInput
-  }
-}
-</script>
 
-<style>
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
