ffe083ccb0efd159d10a09bc4ccae3f02bf26452
arypru <arapruy@gmail.com>
2023-08-28 19:42:15 -0300

    FRONTEND: Add Label Input

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,18 +3,21 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <v-titulo Titulo="Transferencias"></v-titulo>
     <v-subtitulo subtitulo="Prueba de transferencia"></v-subtitulo>
+    <v-label-input textoLabel="prueba label input"></v-label-input>
   </div>
 </template>
 
 <script>
 import VTitulo from "@/components/VTitulo";
 import VSubtitulo from "@/components/VSubtitulo";
+import VLabelInput from "@/components/VLabelInput"
 
 export default {
   name: 'App',
   components: {
     VTitulo,
-    VSubtitulo
+    VSubtitulo,
+    VLabelInput
   }
 }
 </script>
