ecf3cac63e4385138cfa70ab4479382b2369e3f6
arypru <arapruy@gmail.com>
2023-08-28 21:39:49 -0300

    FRONTEND: Add Input

---
+++ frontend/src/components/VTextFile.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-text-field
+      outlined
+      label="Prepend inner"
+      prepend-inner-icon="mdi-map-marker"
+      color="amber accent-4"
+  ></v-text-field>
+</template>
+
+<script>
+export default {
+  name: 'TextFile'
+}
+</script>
