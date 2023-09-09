168a140a6fe738b05b5e012025d04ed3975b3c67
arypru <arapruy@gmail.com>
2023-09-09 15:15:03 -0300

    FRONTEND: Crear Tarjetas Section

--- frontend/src/views/VHome.vue
+++ frontend/src/views/VHome.vue
@@ -1,35 +1,21 @@
 <template>
   <div>
     <v-navbar/>
-
     <v-container rounded-xl style="background: white" class="mt-4 py-8 px-13">
       <v-section-transferencia/>
       <v-section-products/>
-
-      <div>
-        <v-titulo-home titulo="Tarjetas"/>
-        <div class="d-flex flex-row justify-space-beetwen mt-8">
-          <v-tarjeta-card/>
-          <v-tarjeta-card/>
-          <v-tarjeta-card/>
-        </div>
-      </div>
-
+      <v-section-tarjeta/>
     </v-container>
   </div>
 </template>
-
 
 <script>
 import VNavbar from "../components/VNavbar"
 import VSectionTransferencia from "../components/VSectionTransferencia"
 import VSectionProducts from "../components/VSectionProducts"
-import VTituloHome from "../components/VTituloHome"
-import VTarjetaCard from "../components/VTarjetaCard"
+import VSectionTarjeta from "../components/VSectionTarjeta"
 
 export default {
-  components:{VNavbar,VSectionTransferencia,VSectionProducts,VTituloHome,VTarjetaCard},
-
-
+  components:{VNavbar,VSectionTransferencia,VSectionProducts,VSectionTarjeta},
 }
 </script>
