d961b1148738858ea48b74bd2ba77b5936148c87
arypru <arapruy@gmail.com>
2023-09-13 20:40:47 -0300

    BACKEND: MSFC Tipo Cuenta

---
+++ backend/database/seeders/TipoCuentaSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\TipoCuenta;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TipoCuentaSeeder extends Seeder
+{
+        Private $arrayTipoCuenta = (array(
+        array('acronimo'=>'CA', 'descripcion'=>'Caja de Ahorro', 'moneda_id'=> 1),
+        array('acronimo'=>'CC', 'descripcion'=>'Cuenta Corriente', 'moneda_id'=> 1),
+        array('acronimo'=>'CC(Sueldo)', 'descripcion'=>'Cuenta Sueldo', 'moneda_id'=> 1),
+        array('acronimo'=>'CC', 'descripcion'=>'Cuenta Corriente', 'moneda_id'=> 2),
+        ));
+
+    public function run(): void
+    {
+        foreach ($this->arrayTipoCuenta as $c)
+        {
+            $tc = new TipoCuenta();
+            $tc->acronimo = $c['acronimo'];
+            $tc->descripcion = $c['descripcion'];
+            $tc->moneda_id = $c['moneda_id'];
+            $tc->save();
+        }
+    }
+}
