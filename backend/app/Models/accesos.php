470eb4e1771ac2d678a0090d30f3b4e189a364a2
arypru <arapruy@gmail.com>
2023-09-25 21:55:16 -0300

    BACKEND: Historial sessiones

--- backend/app/Models/accesos.php
+++ backend/app/Models/accesos.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class accesos extends Model
 {
@@ -17,5 +18,23 @@ class accesos extends Model
         return $this->hasOne(User::class);
     }
 
+    public static function buscarDevicesUser($id_user){
+
+        $query = DB::table('accesos')
+            ->select(
+                'accesos.id as accesoId',
+                'accesos.ultima_conexion as ultimaConexion',
+                'accesos.ip as ip',
+                'devices.device_name as servicioNombre',
+                'devices.browser as browser',
+                'devices.os as os',
+            )
+            ->join('devices', 'devices.id', '=', 'accesos.devices_id')
+            ->where('user_id', $id_user)
+            ->orderByDesc('accesos.ultima_conexion')
+            ->get();
+
+        return $query;
+    }
 
 }
