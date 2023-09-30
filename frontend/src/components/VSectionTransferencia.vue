325ebeaa1c565661d8323cd18362cddedf09ec68
arypru <arapruy@gmail.com>
2023-09-30 10:52:40 -0300

    FRONTEND: Transferencia

--- frontend/src/components/VSectionTransferencia.vue
+++ frontend/src/components/VSectionTransferencia.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="p-12">
     <v-titulo-home titulo="Transferencias"/>
-    <div class="d-flex flex-row justify-space-between align-center my-8">
+    <div class="d-flex flex-wrap justify-sm-center justify-md-space-between align-center my-8">
       <div v-for="(item, i) in itemsCard" :key="i">
-        <v-boton-card :link="item.link" :texto="item.text" :imagenurl="item.imagenurl"/>
+        <v-boton-card class="my-sm-3" :link="item.link" :texto="item.text" :imagenurl="item.imagenurl"/>
       </div>
     </div>
   </div>
@@ -16,6 +16,7 @@ import VTituloHome from "../components/VTituloHome"
 import newtransfer from "../assets/images/transferencia-bancaria.svg";
 import vertransfer from "../assets/images/factura.svg";
 import verbenef from "../assets/images/cuenta-de-usuario.svg"
+import {mapGetters} from "vuex";
 
 export default {
   components:{VBotonCard,VTituloHome},
@@ -24,11 +25,15 @@ export default {
     return {
       itemsCard: [
         { text: `Nueva` + `\n` + `Transferencia`, imagenurl: newtransfer, link: '/nueva-transferencia' },
-        { text: `Ver` + `\n` + `Transferencias`, imagenurl: vertransfer, link: ''},
+        { text: `Ver` + `\n` + `Transferencias`, imagenurl: vertransfer, link: '/transferencias/'+`${this.$store.getters.user.id}`},
         { text: `Ver` + `\n` + `Beneficiarios`, imagenurl: verbenef, link:''},
       ],
     }
-  }
+  },
+
+  computed: {
+    ...mapGetters(['user']),
+  },
 
 }
 </script>
